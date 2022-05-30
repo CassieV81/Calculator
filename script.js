6dc5c9f550250c47faf01f212b0fe2aad2a96880
CassieV <obicassievirtue@gmail.com>
2022-05-30 19:15:31 +0200

    Creat function to populate the display

--- script.js
+++ script.js
@@ -7,4 +7,32 @@ function operate(num1, num2) {
     return add(num1, num2);
 }
 
+const number = document.querySelectorAll('.number');
+const display = document.getElementById('display');
+const addValue = document.querySelector('.add');
+
+let value = '';
+
+function populate() {
+    number.forEach(number => {
+        number.addEventListener('click', function() {
+            value += number.textContent;
+            display.textContent = `${value}`;
+        });
+    })
+}
+function calculate() {
+    addValue.addEventListener('click', function(num1, num2) {
+        console.log(num1 = display.textContent);
+        operate(num1, num2);
+        return value = '';
+    })
+}
+calculate()
+
+
+populate()
+console.log(`${value}`);
+
+
 console.log(operate(3, 5));
