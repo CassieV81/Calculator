2a3b6294d7435c71c1bd8d38eb1862fc452f91c0
CassieV <obicassievirtue@gmail.com>
2022-05-30 21:20:26 +0200

    Create a function that adds to values when clicked

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+let num1, num2 = ''
 
-function operate(num1, num2) {
+function operate() {
     return add(num1, num2);
 }
 
 const number = document.querySelectorAll('.number');
 const display = document.getElementById('display');
 const addValue = document.querySelector('.add');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
 
 let value = '';
+
 
 function populate() {
     number.forEach(number => {
@@ -21,14 +25,25 @@ function populate() {
         });
     })
 }
-function calculate() {
-    addValue.addEventListener('click', function(num1, num2) {
-        console.log(num1 = display.textContent);
-        operate(num1, num2);
-        return value = '';
-    })
+function clearDisplay() {
+    clear.addEventListener('click', () => {
+        display.textContent = '';
+        value = '';
+    });
 }
-calculate()
+function calculate() {
+    addValue.addEventListener('click', function() {
+        console.log(num1 = Number(display.textContent));
+        return value = '';   
+    });
+    equals.addEventListener('click', function() {
+        console.log(num2 = Number(display.textContent));
+        console.log(num1, num2)
+        display.textContent = operate(num1, num2);
+    });
+}
+clearDisplay();
+calculate();
 
 
 populate()
